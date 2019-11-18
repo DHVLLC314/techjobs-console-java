df07bb4982901780e5cf8b4469449669c76623b6
Dream Higher Visions <dreamhighervisions@Dreams-MacBook-Air.local>
2019-11-18 04:53:41 -0600

    Completed Assignment 1

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -38,7 +39,7 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
             String aValue = row.get(field);
 
-            if (!values.contains(aValue)) {
+            if (!values.toString().toLowerCase().contains(aValue.toLowerCase())) {
                 values.add(aValue);
             }
         }
@@ -76,7 +77,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -124,5 +125,21 @@ public class JobData {
             e.printStackTrace();
         }
     }
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> job = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            if (row.toString().toLowerCase().contains(value.toLowerCase())) {
+                if (!job.contains(row)) {
+                    job.add(row);
+                }
+            }
+        }
+        return job;
+    }
+
 
 }
